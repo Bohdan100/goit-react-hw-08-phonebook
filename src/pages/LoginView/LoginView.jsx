886ec225341f4861a9d569bc862ed044f7531df0
Bohdan <b.orlovskiy@gmail.com>
2022-12-11 12:54:47 +0200

    commit

--- src/pages/LoginView/LoginView.jsx
+++ src/pages/LoginView/LoginView.jsx
@@ -1,5 +1,61 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from 'redux/authorization';
+
+import { LoginForm, LoginLabel } from './LoginView.styled';
+
 const LoginView = () => {
-  return <div>LoginView</div>;
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(authOperations.logIn({ email, password }));
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      <h1>Страница логина</h1>
+
+      <LoginForm onSubmit={handleSubmit} autoComplete="off">
+        <LoginLabel>
+          Почта
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </LoginLabel>
+
+        <LoginLabel>
+          Пароль
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </LoginLabel>
+
+        <button type="submit">Войти</button>
+      </LoginForm>
+    </div>
+  );
 };
 
 export default LoginView;
---
+++ src/pages/LoginView/LoginView.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const LoginForm = styled.form`
+  width: 320px;
+`;
+
+export const LoginLabel = styled.label`
+  display: inline-flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+`;
