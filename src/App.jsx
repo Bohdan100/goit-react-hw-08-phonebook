457ed1e8d68f20612118fcaa091c0a154a0b77cf
Bohdan <b.orlovskiy@gmail.com>
2022-12-12 15:21:09 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import AppBar from './pages/AppBar';
 import RegisterView from './pages/RegisterView';
 import LoginView from './pages/LoginView';
 import PhonebookView from './pages/PhonebookView';
+import PrivateRoute from './components/Routes/PrivateRoute';
+import PublicRoute from './components/Routes/PublicRoute';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -18,15 +20,24 @@ export const App = () => {
   }, [dispatch]);
   return (
     <Routes>
-      <Route path="/" element={<AppBar />}>
+      <PublicRoute path="/">
+        <AppBar />
         <Route
           index
           element={<ImAddressBook size={400} fill="#64b5f6" opacity={0.8} />}
         />
-        <Route path="/register" element={<RegisterView />} />
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/contacts" element={<PhonebookView />} />
-      </Route>
+
+        <PublicRoute path="/register" restricted>
+          <RegisterView />
+        </PublicRoute>
+        <PublicRoute path="/login" restricted>
+          <LoginView />
+        </PublicRoute>
+
+        <PrivateRoute path="/contacts" redirectTo="/login">
+          <PhonebookView />
+        </PrivateRoute>
+      </PublicRoute>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
