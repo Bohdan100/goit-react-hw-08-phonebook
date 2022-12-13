b1ba4d7263e4f0f935e4d78a7081957801111d23
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2022-12-13 02:24:27 +0200

    commit

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,30 +2,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { authSelectors, authOperations } from 'redux/authorization';
 // import defaultAvatar from './default-avatar.png';
 
-const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  name: {
-    fontWeight: 700,
-    marginRight: 12,
-  },
-};
-
+import { UserMenuWrapper, UserMenuText, ExitBtn } from './UserMenu.styled';
 const UserMenu = () => {
   const dispatch = useDispatch();
   const name = useSelector(authSelectors.getUsername);
   // const avatar = defaultAvatar;
 
   return (
-    <div style={styles.container}>
+    <UserMenuWrapper>
       {/* <img src={avatar} alt="" width="32" /> */}
-      <span style={styles.name}>Добро пожаловать, {name}</span>
-      <button type="button" onClick={() => dispatch(authOperations.logOut())}>
-        Выйти
-      </button>
-    </div>
+      <UserMenuText>Welcome {name}</UserMenuText>
+      <ExitBtn type="button" onClick={() => dispatch(authOperations.logOut())}>
+        Exit
+      </ExitBtn>
+    </UserMenuWrapper>
   );
 };
 
--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -1,31 +1,30 @@
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
 
-export const NavigationItem = styled.li`
-  margin-right: 20px;
-  padding: 12px;
+export const UserMenuWrapper = styled.span`
+  display: flex;
+  align-items: center;
+`;
+
+export const UserMenuText = styled.span`
+  margin-right: 12px;
+  font-weight: ${p => p.theme.fontWeights.bold};
+`;
+
+export const ExitBtn = styled.li`
+  min-width: 50px;
+  margin-right: 15px;
+  padding: 10px;
+  border: 1px solid ${p => p.theme.colors.indigo};
   border-radius: ${p => p.theme.radii.border};
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
 
+  color: ${p => p.theme.colors.black};
   font-weight: ${p => p.theme.fontWeights.bold};
-  list-style: none;
-
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   :hover {
     background-color: ${p => p.theme.colors.blueFirst};
     cursor: pointer;
-  }
-`;
-
-export const NavigationLink = styled(NavLink)`
-  padding: 15px;
-  text-decoration: none;
-
-  color: ${p => p.theme.colors.white};
-
-  &.active {
-    color: ${p => p.theme.colors.orange};
   }
 `;
