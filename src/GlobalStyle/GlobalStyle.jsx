882fa64044274ce62e63f1ba3bb34ef44d4743f4
Bohdan <b.orlovskiy@gmail.com>
2022-12-15 12:56:37 +0200

    commit

--- src/GlobalStyle/GlobalStyle.jsx
+++ src/GlobalStyle/GlobalStyle.jsx
@@ -1,44 +1,35 @@
-import { Global, css } from '@emotion/react';
+import { createGlobalStyle } from 'styled-components';
 import 'modern-normalize';
 // npm i modern-normalize
 
-export const GlobalStyle = () => (
-  <Global
-    styles={css`
-      * {
-        box-sizing: border-box;
-      }
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-      body {
-        background-color: '#64b5f6';
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-          'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-          'Helvetica Neue', sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-      }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
 
-      code {
-        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-          monospace;
-      }
+  h1,h2,h3,h4,h5,h6,p {
+    margin: 0;
+  }
 
-      h1,
-      h2,
-      h3,
-      h4,
-      h5,
-      h6,
-      p {
-        margin: 0;
-      }
-
-      img {
-        display: block;
-        max-width: 100%;
-        height: auto;
-      }
-    `}
-  />
-);
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+`;
